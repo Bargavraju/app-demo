e118c2b9dd554b52ab2b27d797098841b2942f48
Bargaraju <bargav1014@gmail.com>
2026-02-05 16:40:59 +0530

    Added main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return "Hello from Application CI/CD!"
 
+
 @app.route("/health")
 def health():
     return "OK"
+
 
 if __name__ == "__main__":
     app.run()
