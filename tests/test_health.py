72a10925aca7c5deea1b40504b3430cfe72ea21f
Bargaraju <bargav1014@gmail.com>
2026-02-06 11:21:02 +0530

    Fix flake lint issues

--- tests/test_health.py
+++ tests/test_health.py
@@ -1,13 +1,9 @@
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 from main import app
 
 
 def test_health():
     client = app.test_client()
     response = client.get("/health")
+
     assert response.status_code == 200
     assert response.data == b"OK"
